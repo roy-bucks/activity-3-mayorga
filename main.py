eada74f015fcf7058015501aba85a6438f1e7a90
Roy <roymayorga10@gmail.com>
2022-03-31 13:45:56 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,56 @@
 import stat_cal as stat
+import ev_calc as ev
 
 #This will compute the HP and the other stat
-def stat_calc(): 
-    print("I am going to calculate the HP and other stat")
+def stat_calc(stat_data): 
+    #compute the HP 
+    total_hp = stat.compute.hp(stat_data)
+    print("The total hp is: ")
+    print(total_hp)
+
+    #compute the other statistics 
+    #NOT CLEAR YET
+    other_stat = stat.compute.other_stat(stat_data)
+    print("The other stat is: ")
+    print(other_stat)
+
+    option(stat_data)
+
+#compute the ev needed
+def ev_calc(stat_data): 
+    print("You are about to compute the ev's needed to increase stat")
+    print("please enter your desired stat increase")    
+    stat = int(input())
+    stat_data["stat"] = stat
+
+    eve_needed = ev.compute.ev_needed(stat_data)
+    print("The ev needed is: ")
+    print(eve_needed)
+
+    option(stat_data)
+
+
+#Option for type of computation
+def computation_type(data):
+    print("Please choose type of calculation")
+    print("1. Stat calculation")
+    print("2. Ev calculation")
+
+    opt = int(input())
+
+    if(opt == 1):
+        stat_calc(data)
+
+    elif(opt == 2):
+        ev_calc(data)
+    else:
+        print("Invalid input") 
+
+#First step1
+#user chose between ev calculation and stat calculation 
+def start():
+
+    print("I need some information to proceed on calculation")
     print("Please enter the following input")
 
     print("please enter the base hp")
@@ -27,40 +75,44 @@ def stat_calc():
     level = int(input())
 
 
+    print("pleease type of nature")
+    print("1. Benificial")
+    print("2. Hindering")
+    natureInput = int(input())
+
+    if natureInput == 1:
+        nature = 1.1
+    elif natureInput == 2:
+        nature = 0.9
+    else: 
+        nature = 1.1
+
+
     stat_data = {
         "base": base,
         "iv": iv,
         "ev": ev,
-        "level": level
+        "level": level,
+        "nature": nature
     }
 
-    total_hp = stat.compute.hp(stat_data)
-    print("The total hp is: ")
-    print(total_hp)
-
-def ev_calc(): 
-    print("I am goint to ev calc")
-    
+    computation_type(stat_data)
 
 
-#First step1
-#user chose between ev calculation and stat calculation 
-def start():
-    print("Please choose type of calculation")
-    print("1. Stat calculation")
-    print("2. Ev calculation")
 
-    opt = int(input())
+#computation end prompt
+def option(data): 
+    print("1. To choose other computation")
+    print("2. To start again")
 
-    if(opt == 1):
-        stat_calc()
+    x = int(input())
 
-    elif(opt == 2):
-        ev_calc()
-
+    if( x == 1): 
+        computation_type(data)
+    elif( x == 2):
+        start()
     else:
-        print("Invalid input") 
-
+        start()
 
 
 start()
--- stat_cal.py
+++ stat_cal.py
@@ -4,3 +4,8 @@ class compute():
     def hp(data):
         total_hp = (((2 * data["base"] + data["iv"] + (data["ev"]/4))* data["level"])/100) + (data["level"] + 10)
         return total_hp
+    
+    # compute the other stat
+    def other_stat(data):
+        other_stat = ((((2 * data['base'] + data['iv'] + (data['ev']/4)) * data["level"])/100) + 5) * data["nature"]
+        return other_stat
